cbfe381bc2304eb39ce63b3d818bc3e720b47076
Stefan Istoc <84193298+Sainthub@users.noreply.github.com>
2021-12-09 23:48:40 +0100

    Add files via upload

---
+++ index.js
@@ -0,0 +1,39 @@
+const PORT = 8000
+const axios = require('axios')
+const cheerio = require('cheerio')
+const express = require('express')
+const app = express()
+const cors = require('cors')
+app.use(cors())
+
+const url = 'https://www.nytimes.com/'
+
+app.get('/', function (req, res) {
+    res.json('This is my webscraper')
+})
+
+app.get('/results', (req, res) => {
+    axios(url)
+        .then(response => {
+            const html = response.data
+            const $ = cheerio.load(html)
+            const articles = []
+
+            // .story-wrapper is our element in the html. So we are going to grab it and then inspect it
+
+            $('.story-wrapper', html).each(function () { //<-- cannot be a function expression
+                const title = $(this).text()
+                const url = $(this).find('a').attr('href')
+                articles.push({
+                    title,
+                    url
+                })
+            })
+            res.json(articles)
+        }).catch(err => console.log(err))
+
+})
+
+
+app.listen(PORT, () => console.log(`server running on PORT ${PORT}`))
+
